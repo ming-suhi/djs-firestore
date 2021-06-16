a68efe523906c8fb8c6d159c492630509f7d5446
明suhi <61369062+ming-suhi@users.noreply.github.com>
2021-06-16 11:50:26 +0800

    Change config from .json to .env
    .env files is commonly used by web hosted apps.

--- src/client.js
+++ src/client.js
@@ -1,27 +1,38 @@
 const FirebaseAdmin = require('firebase-admin');
 const DatabaseManager = require('./managers/database.manager.js');
 const {LocalClient} = require('../djs-local-manager/src/index.js');
+const dotenv = require('dotenv');
 
 
 class Client {
 
   /**
    * Client for managing app client, and db client
-   * @param {string} config path to config.json from root
-   * @property {object} config access config.json as object
+   * @property {string} token access bot token
    * @property {LocalClient} local class for managing local
    * @property {DatabaseManager} db class for managing database
    */
-  constructor(config) {
-    
-    this.config = require.main.require(config);
+  constructor() {
+    dotenv.config();
 
     //initializes firebase app to use db
 
     FirebaseAdmin.initializeApp({
-      credential: FirebaseAdmin.credential.cert(this.config.service_account)
+      credential: FirebaseAdmin.credential.cert({
+        "type": "service_account",
+        "project_id": process.env.PROJECT_ID,
+        "private_key_id": process.env.PRIVATE_KEY_ID,
+        "private_key": process.env.PRIVATE_KEY,
+        "client_email": process.env.CLIENT_EMAIL,
+        "client_id": process.env.CLIENT_ID,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
+      })
     });
 
+    this.token = process.env.BOT_TOKEN;
     this.local = new LocalClient();
     this.db = new DatabaseManager();
   }
