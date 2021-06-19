9ff53c19b4dca1b04a7120ffec4dcb7422237e79
明suhi <61369062+ming-suhi@users.noreply.github.com>
2021-06-19 18:31:33 +0800

    Add submodule exports to main
    Added submodule to package main, to allow users to export submodule classes.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+const LocalManager = require('../djs-local-manager/src/index.js');
+
 module.exports = {
   "Client": require('./client.js'),
   "Collection": require('./structures/collection.js'),
-  "Document": require('./structures/document.js')
+  "Document": require('./structures/document.js'),
+  ...LocalManager
 }
