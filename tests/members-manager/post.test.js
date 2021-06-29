01ce0a02a98c31e4b1cecadd5dd0ae6f7d03ca2a
明suhi <k.j.b.taton@gmail.com>
2021-06-29 14:23:45 +0800

    Add tests
    Added tests for MemberManager class

--- tests/members-manager/post.test.js
+++ tests/members-manager/post.test.js
@@ -4,7 +4,7 @@ const client = new Client();
 const guild_object = {"id": "test"}
 const member_object = {"id": "test2", "name": "test2", "archived": false}
 
-test('post guild', async() => {
+test('post member', async() => {
   const guild = await client.db.guilds.fetch(guild_object.id);
   await guild.members.post(member_object);
   const member = await guild.members.get(member_object.id);
