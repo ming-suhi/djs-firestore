7c9eb4a4d4fe210dba201ece4edac2e748fc36ac
明suhi <k.j.b.taton@gmail.com>
2021-08-08 18:37:53 +0800

    Fix(FirestoreManager): Parse private key

--- src/client.ts
+++ src/client.ts
@@ -15,7 +15,7 @@ export class FirestoreManager {
     const serviceAccount: ServiceAccount = {
       projectId: process.env.PROJECT_ID,
       clientEmail: process.env.CLIENT_EMAIL,
-      privateKey: process.env.PRIVATE_KEY
+      privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n')
     }
 
     FirebaseAdmin.initializeApp({
