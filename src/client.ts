4fb0fa356f3e7df4b85a537a7079bd63405b326f
明suhi <k.j.b.taton@gmail.com>
2021-09-04 15:47:20 +0800

    Feature(FirestoreManager): Add initialize

--- src/client.ts
+++ src/client.ts
@@ -1,12 +1,16 @@
 import FirebaseAdmin, { ServiceAccount } from 'firebase-admin';
+import { Client, Collection, OAuth2Guild } from 'discord.js';
 import dotenv from 'dotenv';
+import { GuildDocument } from '.';
 
 dotenv.config();
 
 /** Main structure for managing database */
 export class FirestoreManager {
-  constructor() {
+  /** Guild documents */
+  public guilds: Map<string, any> = new Map;
 
+  constructor() {
     const serviceAccount: ServiceAccount = {
       projectId: process.env.PROJECT_ID,
       clientEmail: process.env.CLIENT_EMAIL,
@@ -15,6 +19,19 @@ export class FirestoreManager {
 
     FirebaseAdmin.initializeApp({
       credential: FirebaseAdmin.credential.cert(serviceAccount)
+    })
+  }
+
+  /**
+   * Initialize guilds database
+   */
+  initializeGuilds(client: Client) {
+    client.guilds.fetch().then((guilds: Collection<string, OAuth2Guild>) => {
+      guilds.forEach((guild: OAuth2Guild) => {
+        const document = new GuildDocument(guild.id);
+        document.initialize()
+        this.guilds.set(guild.id, document);
+      })
     })
   }
 }
--- src/structures/firestore.ts
+++ src/structures/firestore.ts
@@ -1,5 +1,5 @@
 import { firestore } from 'firebase-admin';
-import { DocumentReference, DocumentData, DocumentSnapshot, WriteResult, Timestamp } from '@google-cloud/firestore';
+import { DocumentReference, DocumentData, DocumentSnapshot } from '@google-cloud/firestore';
 
 /** Structure for managing a specific document */
 export class Document {
@@ -8,7 +8,7 @@ export class Document {
   /** Document data */
   protected data?: DocumentData;
   /** Document last write */
-  protected deleted: boolean = false;
+  private _deleted: boolean = false;
 
   /**
    * @param path Document absolute path
@@ -29,27 +29,34 @@ export class Document {
   }
 
   /**
+   * If document was deleted
+   */
+  public get deleted(): boolean {
+    return this._deleted;
+  } 
+
+  /**
    * Get document field
    * @param name Field name
    * @returns Field value
    */
-  public async get(name: string) {
-    return this.data![name];
+  public get(name: string) {
+    return this.data?.[name];
   }
 
   /**
    * Update document field
    * @param data Document data
    */
-  async update(data: any) {
+  public async update(data: any) {
     await this.reference.set(data, { merge: true });
   }
 
   /**
    * Delete document
    */
-  async delete() {
-    this.deleted = true;
+  public async delete() {
     await this.reference.delete();
+    this._deleted = true;
   }
 }
